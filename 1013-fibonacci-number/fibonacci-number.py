1b9d278fb27bda880b2f41c86e65dbf74db51c30
Sudhanshub27 <159228884+Sudhanshub27@users.noreply.github.com>
2025-08-04 22:51:16 +0530

    Time: 347 ms (26.35%) | Memory: 17.6 MB (63.72%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -4,5 +4,4 @@ class Solution:
             return 0
         if n==1:
             return 1
-        if n>1:
-            return self.fib(n-1) +self.fib(n-2)
+        return self.fib(n-1) +self.fib(n-2)
