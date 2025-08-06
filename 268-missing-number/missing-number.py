89508c14253e48db699eeb48447abfded54ada3c
Sudhanshub27 <159228884+Sudhanshub27@users.noreply.github.com>
2025-08-06 21:44:24 +0530

    Time: 4 ms (47.74%) | Memory: 18.7 MB (71.78%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,10 +1,15 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        # n=len(nums) tc=O(N2)
+        # for i in range(n+1):
+        #     if i in nums:
+        #         continue
+        #     else:
+        #         return i
+        # better approach is to use formula
         n=len(nums)
-        for i in range(n+1):
-            if i in nums:
-                continue
-            else:
-                return i
+        total_sum=n*(n+1)//2
+        actual_sum=sum(nums)
+        return total_sum-actual_sum
 
         
