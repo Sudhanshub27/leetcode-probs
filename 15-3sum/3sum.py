330b8cf39b40f7c45ede9dab99ad1d1394983c99
Sudhanshub27 <159228884+Sudhanshub27@users.noreply.github.com>
2025-08-07 22:52:35 +0530

    Time: 1493 ms (9.40%) | Memory: 21.8 MB (10.40%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -12,35 +12,23 @@ class Solution:
         #                 continue
         # return [list(i) for i in result]
         nums.sort()
-        result = []
-        n = len(nums)
-
+        result=set()
+        n=len(nums)
         for i in range(n):
-            # Skip duplicates for i
-            if i > 0 and nums[i] == nums[i-1]:
-                continue
-
-            left = i + 1
-            right = n - 1
-
-            while left < right:
-                total = nums[i] + nums[left] + nums[right]
-
-                if total == 0:
-                    result.append([nums[i], nums[left], nums[right]])
-                    left += 1
-                    right -= 1
-                    # Skip duplicates for left and right
-                    while left < right and nums[left] == nums[left - 1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right + 1]:
-                        right -= 1
-                elif total < 0:
-                    left += 1
+            left=i+1
+            right=n-1
+            while left<right:
+                total=nums[i]+nums[left]+nums[right]
+                if total==0:
+                    ans=(nums[i],nums[left],nums[right])
+                    result.add(ans)
+                    left+=1
+                    right-=1
+                elif total<0:
+                    left+=1
                 else:
-                    right -= 1
-
-        return result
+                    right-=1
+        return [list(i) for i in result]
 
 
 
