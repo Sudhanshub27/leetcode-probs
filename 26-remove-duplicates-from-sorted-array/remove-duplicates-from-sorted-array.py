5f65836da19209633f5d391b59891cf51425820c
Sudhanshub27 <159228884+Sudhanshub27@users.noreply.github.com>
2025-08-10 00:02:48 +0530

    Time: 0 ms (100.00%) | Memory: 18.9 MB (74.21%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -1,3 +1,4 @@
+import itertools
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         # result=set()
@@ -8,17 +9,22 @@ class Solution:
         #     nums.append(i)
         # nums.sort()
         # return len(nums)
-        map={}
-        n=len(nums)
-        for i in nums:
-            if i in map:
-                map[i]+=1
-            else:
-                map[i]=1
-        nums.clear()
-        for key,val in map.items():
-            nums.append(key)
+        # map={}
+        # n=len(nums)
+        # for i in nums:
+        #     if i in map:
+        #         map[i]+=1
+        #     else:
+        #         map[i]=1
+        # nums.clear()
+        # for key,val in map.items():
+        #     nums.append(key)
+        # return len(nums)
+     
+
+        nums[:] = [key for key, _ in itertools.groupby(nums)]
         return len(nums)
+
 
         
 
