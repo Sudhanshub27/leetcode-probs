fcf53575526dc4d70feebd0c725213f256f6dab6
Sudhanshub27 <159228884+Sudhanshub27@users.noreply.github.com>
2025-09-03 23:47:46 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (30.64%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -1,11 +1,8 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        number=""
-        for i in digits:
-            str_i=str(i)
-            number=number+str_i
-        int_num=int(number)+1
+        number = "".join(str(i) for i in digits)
         result=[]
+        int_num=int(number)+1
         while int_num>0:
             n=int_num%10
             result.append(n)
