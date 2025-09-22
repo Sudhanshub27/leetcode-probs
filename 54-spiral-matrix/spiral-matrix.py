3fad9c51158c523a85e844d9537ed69b21f2bc1f
Sudhanshub27 <159228884+Sudhanshub27@users.noreply.github.com>
2025-09-22 22:37:40 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (18.74%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -1,17 +1,50 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        rows, cols = len(matrix), len(matrix[0])
-        x, y, dx, dy = 0, 0, 1, 0
-        res = []
+        m=len(matrix)
+        n=len(matrix[0])
+        ans=[]
+        i,j=0,0
+        UP,RIGHT,DOWN,LEFT=0,1,2,3
+        direction=RIGHT
 
-        for _ in range(rows * cols):
-            res.append(matrix[y][x])
-            matrix[y][x] = "."
+        UP_WALL=0
+        RIGHT_WALL=n
+        DOWN_WALL=m
+        LEFT_WALL=-1
 
-            if not 0 <= x + dx < cols or not 0 <= y + dy < rows or matrix[y+dy][x+dx] == ".":
-                dx, dy = -dy, dx
-            
-            x += dx
-            y += dy
+        while len(ans)!=(m*n):
+            if direction == RIGHT:
+                while j<RIGHT_WALL:
+                    ans.append(matrix[i][j])
+                    j+=1
+                i=i+1
+                j=j-1
+                RIGHT_WALL-=1
+                direction=DOWN
+            elif direction == DOWN:
+                while i<DOWN_WALL:
+                    ans.append(matrix[i][j])
+                    i+=1
+                i=i-1
+                j=j-1
+                DOWN_WALL-=1
+                direction =LEFT
+            elif direction==LEFT:
+                while j>LEFT_WALL:
+                    ans.append(matrix[i][j])
+                    j-=1
+                i,j=i-1,j+1
+                LEFT_WALL+=1
+                direction=UP
+            else:
+                while i>UP_WALL:
+                    ans.append(matrix[i][j])
+                    i-=1
+                i=i+1
+                j=j+1
+                UP_WALL+=1
+                direction=RIGHT
+        return ans
+
+
         
-        return res
